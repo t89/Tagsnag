8e34fbd66d0ecf962f4cf043d78f244a42e9dee1
Thomas Johannesmeyer <thomas.johannesmeyer@me.com>
2018-11-01 06:16:16 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,23 @@
+##
+#!/usr/bin/python
+#
+#  main.py
+#  Tagsnatch
+#
+#  Created by Thomas Johannesmeyer (thomas@geeky.gent) on 31.10.2018.
+#  Copyright (c) 2018 www.geeky.gent. All rights reserved.
+#
+
+
+import sys
+
+def main(argv):
+    try:
+        print("{}".format("Hello Professor Falken!"))
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
